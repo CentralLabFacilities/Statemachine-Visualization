8870c16f29705f256d8dee12bced7c7685ca40cd
Slothologist <rfeldhans@techfak.uni-bielefeld.de>
2017-02-21 13:05:29 +0100

    changed comments to docstrings; re-ordering of methods and startup/switch-setup

--- sm-viz.py
+++ sm-viz.py
@@ -5,56 +5,134 @@ import sys
 # TODO's
 ## readGraph and build_mini_sg need to return a tuple of the graph and a list of tuples which contain edges and node from which these edges derive
 
+"""This is a little program written to visualize statemachines in scxml with the help of GraphViz. 
 
-# help text. if you change things here the amount of tabs may be in need of adjustments.
+Most if not all of the functions and variables should be documented according to the Google Python Style Guide.
+
+Written by Slothologist.
+"""
+
+############################ variables ####################################
+
 help_text= "\
 Usage: sm-viz.py your-statemachine.scxml\n\
 Possible switches:\n\t\
---h \t\t Displays this very(!) helpful text. \n\t\
+--h \t\t Displays this very helpful text. \n\t\
 --ex \t\t Exclude Substatemachines in the generated graph. Actually reduces them to single states. Use this to make your graph more readable. \n\t\
 --reduce=n \t Exclude all Substatemachines below n levels. Use this to make your graph more readable without sacrificing inormation. \n\t\
---bw \t\t Will render the graph without colors (in black and white). \n\t\
+--bw \t\t Will render the graph without colors (aka in black and white). \n\t\
 --format=fmt \t Will render the graph in the specified format. Available formats are: png (default), pdf etc.\n\t\
 --savegv \t Will save the generated GraphViz code. \n\t\
 --gvname=name \t Will save the generated Graphviz code under the given name. Default name is the same as your input (but with extension .gv) Use with -savegv \n\t\
 --rengine=ngin \t Will use the specified engine (ngin) to render the graph \n\t\
---nosubstates \t Similarly to ex and reduce this will suppress states in states. \
+--nocmpstates \t Similarly to ex and reduce this will suppress compound states (states in states). \
 \n"
+"""str: Helping text. If you change things here the amount of tabs (after the flags) may be in need of adjustments.
+"""
 
-############################ variables ####################################
-
-# Specifies the name of the input file.
 input_name = ""
+"""str: Specifies the name of the input file.
+"""
 
-# Namespace of the statemachine. Needs to be 
 ns = "{http://www.w3.org/2005/07/scxml}"
+"""str: Namespace of the statemachine. Will be calculated again when reading the first xml file.
+"""
 
 
 ############################# switches ####################################
 
-# Amount of recursive steps to which substate machines will be included.
 subst_recs = float('inf')
+"""float: Amount of recursive steps to which substate machines will be included. Usage as int, but only float supports infty.
+"""
 
-# Controls whether to use auto colours or a black and white graph.
 color = True
+"""bool: Controls whether to use auto colours or a black and white graph.
+"""
 
-# Controls the format of the output.
 fmt = "png"
+"""str: Controls the format of the output.
+"""
 
-# Controls if the generated GraphViz code is saved.
 savegv = False
+"""bool: Controls if the generated GraphViz code is saved.
+"""
 
-# Controls the filename of the saved GraphViz code.
 gvname = ""
+"""str: Controls the filename of the saved GraphViz code.
+"""
 
-# Controls the engine used to render the graph.
 rengine = ""
+"""str: Controls the engine used to render the graph.
+"""
 
-# Controls if states in states (aka mini-subgraphs) should be rendered in full.
 minisg = True
+"""bool: Controls if states in states (aka mini-subgraphs) should be rendered in full.
+"""
+
+############################### methods #####################################
+
+def readGraph(filename, level=0, body=[], label=""):
+"""Reads a graph from a specified filename.
+
+	Args:
+		filename (str): The name of the file in which the graph 
+		level (int, optional): Used for sub-statemachine purposes, this corresponds to the level in which the graph lies. If greater than subst_recs given at the start of the programm, this graph will be reduced to just one state (but will still have all edges).
+		body (list[str], optional): Used to set the style of the body of this graph. Contains of a list of str.
+		label (str, optional): Used to set the label of the graph.
+	Returns:
+		Digraph: The Digraph of the specified file.
+"""
+	# prepare return graph
+	g = Digraph('g', engine=rengine)
+
+	# prepare xml tree
+	tree = ET.parse(filename)
+	root = tree.getroot()
+	initial_state = root_node.attrib['initial']
+	send_events = []
+	edges = []
+
+	for child in root:
+		if child.tag.endswith("state"):
+			if "initial" in child.attrib:
+				print("generate mini-subgraph")
+			elif "src" in child.attrib:
+				print("generate true subgraph")
+			elif "parallel" in child.attrib:
+				print("draw parallel subgraph")
+			if "":
+				pass
+	if label:
+		g.body.append('label = \"' + label + '\"')
+	return g
+
+def build_mini_sg(root, label=""):
+"""Builds a subgraph from a specified root node.
+
+	Args:
+		root (Digraph.node): The root node from which this subgraph extends.
+		label (str): Used to label the subgraph.
+
+	Returns:
+		Digraph: The Digraph of the specified rootnode.
+"""
+	pass
+
+def detSubBody(body):
+"""Determins the suitable body for the subgraph of a graph with a given body.
+
+Following order is in use: No body for the main graph.
+
+	Args:
+		body (list[str]): The body of the graph you want to determine the body of the subgraphs of. Contains of a list of str.
+
+	Returns:
+		list[str]: The body of the subgraphs of the graph with the passed body.
+"""
+	pass
 
 
-######################## start and flag stuff ##############################
+######################## startup and flag stuff ##############################
 
 if len(sys.argv) < 2:
 	print(help_text)
@@ -92,7 +170,7 @@ for each in sys.argv:
 			print("Specified engine not known! Will now exit.\n")
 			exit()
 		rengine = each.split("=")[1]
-	elif each == "--nosubstates":
+	elif each == "--nocmpstates":
 		minisg = False
 	else:
 		print("Parameter \"" + each + "\" not recognized. Will now exit.\n")
@@ -110,49 +188,6 @@ else:
 
 if not gvname.endswith(".gv"):
 	gvname = gvname + ".gv"
-
-############################### methods #####################################
-
-# Reads a graph from a specified filename. Returns the Digraph of this File.
-# Optional parameters:
-## level: for sub-statemachine purposes
-## body: for the style of the body of this graph
-## label: to label the graph
-def readGraph(filename, level=0, body=[], label=""):
-	# prepare return graph
-	g = Digraph('g', engine=rengine)
-
-	# prepare xml tree
-	tree = ET.parse(filename)
-	root = tree.getroot()
-	initial_state = root_node.attrib['initial']
-	send_events = []
-
-	for child in root:
-		if child.tag.endswith("state"):
-			if "initial" in child.attrib:
-				print("generate mini-subgraph")
-			elif "src" in child.attrib:
-				print("generate true subgraph")
-			if "":
-				pass
-	if label:
-		g.body.append('label = \"' + label + '\"')
-	return g
-
-# Builds a subgraph from a specified root node. Returns the Digraph of this rootnode.
-# Potional parameters:
-## label: for labeling the subgraph
-def build_mini_sg(root, label=""):
-	pass
-
-
-# Determins the suitable body for the subgraph of a graph with a given body.
-## Following order is in use:
-### no body for the main graph
-#### 
-def detSubBody(body):
-	pass
 
 ####################### start the graph generation ###########################
 
